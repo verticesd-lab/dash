b6e4a35e796fa83f46567c89028791612dc1ba7a
Jhonattan <verticesd@gmail.com>
2025-11-07 11:33:13 -0400

    fix: corrigir darkMode no tailwind.config.ts (string ao invés de array)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   //   "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
   //   "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   // ],
-  darkMode: ["class"],
+  darkMode: "class",
 
   theme: {
     container: {
